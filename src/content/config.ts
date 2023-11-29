efec5f24bcce54ad5c519c91896867530a03faec
Sil <silveltman@gmail.com>
2023-11-29 19:58:56 +0100

    rebuild

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,35 +1,35 @@
 import { defineCollection } from 'astro:content'
 
 const pagesCollection = defineCollection({})
-const postsCollection = defineCollection({
-  /* ... */
-})
-const servicesCollection = defineCollection({
-  /* ... */
-})
-const productsCollection = defineCollection({
-  /* ... */
-})
-const categoriesCollection = defineCollection({
-  /* ... */
-})
-const brandsCollection = defineCollection({
-  /* ... */
-})
-const employeesCollection = defineCollection({
-  /* ... */
-})
-const reviewsCollection = defineCollection({
-  /* ... */
-})
+// const postsCollection = defineCollection({
+//   /* ... */
+// })
+// const servicesCollection = defineCollection({
+//   /* ... */
+// })
+// const productsCollection = defineCollection({
+//   /* ... */
+// })
+// const categoriesCollection = defineCollection({
+//   /* ... */
+// })
+// const brandsCollection = defineCollection({
+//   /* ... */
+// })
+// const employeesCollection = defineCollection({
+//   /* ... */
+// })
+// const reviewsCollection = defineCollection({
+//   /* ... */
+// })
 
 export const collections = {
   pages: pagesCollection,
-  posts: postsCollection,
-  services: servicesCollection,
-  products: productsCollection,
-  categories: categoriesCollection,
-  brands: brandsCollection,
-  employees: employeesCollection,
-  reviews: reviewsCollection,
+  // posts: postsCollection,
+  // services: servicesCollection,
+  // products: productsCollection,
+  // categories: categoriesCollection,
+  // brands: brandsCollection,
+  // employees: employeesCollection,
+  // reviews: reviewsCollection,
 }
