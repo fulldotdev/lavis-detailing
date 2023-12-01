57456a0fb851bbddd112bbfacebf68a118b47bf7
Sil <silveltman@gmail.com>
2023-12-01 18:11:09 +0100

    simplified and ready for cc

--- src/content/pages/index.md
+++ src/content/pages/index.md
@@ -2,17 +2,6 @@
 _schema: default
 blocks:
   - _bookshop_name: blocks/hero
-    section:
-      _bookshop_name: wrappers/section
-      frame:
-      look:
-      layout:
-      x:
-      'y':
-      size:
-      order:
-      hue:
-      scheme:
     writeup:
       _bookshop_name: elements/writeup
       html: <p>Hello there sir</p>
--- types/index.d.ts
+++ types/index.d.ts
@@ -3,6 +3,8 @@ export * from 'astro/types'
 export { VariantProps } from 'cva'
 
 // WRAPPERS
+export * from '@components/wrappers/card/Card.astro'
+export * from '@components/wrappers/deck/Deck.astro'
 export * from '@components/wrappers/group/Group.astro'
 export * from '@components/wrappers/list/List.astro'
 export * from '@components/wrappers/section/Section.astro'
@@ -10,5 +12,17 @@ export * from '@components/wrappers/segment/Segment.astro'
 
 // ELEMENTS
 export * from '@components/elements/button/Button.astro'
+export * from '@components/elements/channel/Channel.astro'
+export * from '@components/elements/collapsible/Collapsible.astro'
+export * from '@components/elements/icon/Icon.astro'
 export * from '@components/elements/image/Image.astro'
+export * from '@components/elements/link/Link.astro'
+export * from '@components/elements/prose/Prose.astro'
+export * from '@components/elements/rating/Rating.astro'
+export * from '@components/elements/social/Social.astro'
+export * from '@components/elements/spec/Spec.astro'
 export * from '@components/elements/writeup/Writeup.astro'
+
+// CARDS
+export * from '@components/blocks/features/FeaturesCard.astro'
+export * from '@components/blocks/offers/OffersCard.astro'
