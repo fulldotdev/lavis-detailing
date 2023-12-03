f7a80d0335ccba4af3fa7187901f343ef81c86e4
Sil <silveltman@gmail.com>
2023-12-03 18:38:59 +0100

    added astro content npath

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import bookshop from '@bookshop/astro-bookshop'
 import robotsTxt from 'astro-robots-txt'
 import { defineConfig } from 'astro/config'
 import UnoCSS from 'unocss/astro'
-import AutoImport from 'unplugin-auto-import/astro'
 
 export default defineConfig({
   output: 'static',
@@ -18,11 +17,6 @@ export default defineConfig({
     bookshop(),
     UnoCSS({
       injectReset: true,
-    }),
-    AutoImport({
-      /* options */
-      include: [/\.astro$/],
-      dirs: ['./types'],
     }),
     // alpine(),
     // favicons({
