053b907d3d8641a994998283324e9feb85abc495
Sil <silveltman@gmail.com>
2023-12-10 19:43:27 +0100

    Remove custom.schema.yml, add stripeProducts.ts, and update ActionsLayout.astro and getProducts.ts

--- src/utils/getProducts.ts
+++ src/utils/getProducts.ts
@@ -1,7 +1,7 @@
 import fs from 'fs'
 import path, { dirname } from 'path'
 import { fileURLToPath } from 'url'
-import { readFrontmatter } from '../../src/utils'
+import { readFrontmatter } from '.'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
