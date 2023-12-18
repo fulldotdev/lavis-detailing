0fb404bd3eb5a2db30d6cec9e87d92ede33dfcd5
Sil <silveltman@gmail.com>
2023-12-18 11:45:45 +0100

    Add .core to .gitignore and merge cardProps with bookshopConfig

--- sync.ts
+++ sync.ts
@@ -1,28 +1,39 @@
 import { execSync } from 'child_process'
 import fs from 'fs'
 
-function removeDirectory(directory) {
+function removeDirectory(directory: string) {
   if (fs.existsSync(directory)) {
     fs.rmSync(directory, { recursive: true, force: true })
   }
 }
 
-function removeFile(filePath) {
+function removeFile(filePath: string) {
   if (fs.existsSync(filePath)) {
     fs.unlinkSync(filePath)
   }
 }
 
-function copyDirectory(sourceDir, destDir) {
+function copyDirectory(sourceDir: string, destDir: string) {
   fs.cpSync(sourceDir, destDir, { recursive: true })
 }
 
-try {
-  // Pull latest changes from core
-  execSync('cd .core git pull', { stdio: 'inherit' })
+function directoryExists(directory: string) {
+  return fs.existsSync(directory)
+}
 
-  // Move all files from .core to the root
-  copyDirectory('.core', '.temp')
+try {
+  // Clone core if it doesn't exist
+  if (!directoryExists('core')) {
+    execSync('git clone https://github.com/silveltman/core.git core', {
+      stdio: 'inherit',
+    })
+  } else {
+    // Pull latest changes from core
+    execSync('cd core git pull', { stdio: 'inherit' })
+  }
+
+  // Move all files from core to the root
+  copyDirectory('core', '.temp')
 
   // Remove specified directories and files
   removeDirectory('.temp/.git')
@@ -30,7 +41,7 @@ try {
   removeDirectory('.temp/src/assets')
   removeFile('.temp/cloudcannon.config.yml')
 
-  // Move all files from .core to the root
+  // Move all files from core to the root
   copyDirectory('.temp', '.')
 
   // Remove temporary directory
