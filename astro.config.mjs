f9ca45f46b1800bcc8cb0c82f6a5eab1f6a4b6b5
Sil <silveltman@gmail.com>
2023-12-18 10:16:21 +0100

    Delete unused files and configurations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,23 @@
-import sitemap from "@astrojs/sitemap";
-import svelte from "@astrojs/svelte";
-import bookshop from "@bookshop/astro-bookshop";
-import yaml from "@rollup/plugin-yaml";
-import robotsTxt from "astro-robots-txt";
-import { defineConfig } from "astro/config";
-import fs from "fs";
-import UnoCSS from "unocss/astro";
+import sitemap from '@astrojs/sitemap'
+import svelte from '@astrojs/svelte'
+import bookshop from '@bookshop/astro-bookshop'
+import yaml from '@rollup/plugin-yaml'
+import robotsTxt from 'astro-robots-txt'
+import { defineConfig } from 'astro/config'
+import UnoCSS from 'unocss/astro'
 
-let company = {};
-try {
-  company = yaml.load(
-    fs.readFileSync("./src/content/globals/company.yml", "utf8")
-  );
-} catch (e) {
-  console.error(e);
-}
+// let company = {}
+// try {
+//   company = yaml.load(
+//     fs.readFileSync('./src/content/globals/company.yml', 'utf8')
+//   )
+// } catch (e) {
+//   console.error(e)
+// }
 
 export default defineConfig({
-  output: "static",
-  site: company.url || "http://localhost:4321",
+  output: 'static',
+  site: 'http://localhost:4321',
   compressHTML: import.meta.env.PROD,
   integrations: [
     svelte(),
@@ -32,4 +31,4 @@ export default defineConfig({
   vite: {
     plugins: [yaml()],
   },
-});
+})
