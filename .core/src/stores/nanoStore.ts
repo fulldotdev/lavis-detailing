ed85a47c3ffa63bd78156c06ead2c1a7bb78f524
Sil <silveltman@gmail.com>
2023-12-18 10:18:03 +0100

    removed .core

--- .core/src/stores/nanoStore.ts
+++
@@ -1,3 +0,0 @@
-import { atom } from 'nanostores'
-
-export const cartIsOpen = atom(false)
--- .core/src/variants/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as hue } from './hue'
-export { default as scheme } from './scheme'
-export { default as size } from './size'
