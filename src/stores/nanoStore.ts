76b975b1c67771cec8058ab177c11598dad2b013
Sil <silveltman@gmail.com>
2023-12-19 11:24:33 +0100

    Remove unused code and fix formatting issues

--- src/stores/nanoStore.ts
+++ src/stores/nanoStore.ts
@@ -1,3 +1,22 @@
-import { atom } from 'nanostores'
+import { persistentAtom } from '@nanostores/persistent'
+import type { CollectionEntry } from 'astro:content'
+import { atom, computed } from 'nanostores'
+
+type Item = CollectionEntry<'products'>['data'] & {
+  quantity: number
+}
+
+export const cart = persistentAtom<Item[] | []>('cart', [], {
+  encode: JSON.stringify,
+  decode: JSON.parse,
+})
 
 export const cartIsOpen = atom(false)
+
+export const totalPrice = computed(cart, ($cart) =>
+  $cart.reduce((a, b) => +a + b.price * b.quantity, 0)
+)
+
+export const totalQuantity = computed(cart, ($cart) =>
+  $cart.reduce((a, b) => +a + b.quantity, 0)
+)
