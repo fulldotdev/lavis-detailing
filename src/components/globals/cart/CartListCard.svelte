ce55271adf6234eda5210ccc26e15fea3de7a1e7
Dylan Kuipers <dylankuipers10@gmail.com>
2023-12-19 14:20:51 +0100

    cart small changes

--- src/components/globals/cart/CartListCard.svelte
+++ src/components/globals/cart/CartListCard.svelte
@@ -10,14 +10,14 @@
     <div class="w-40">
       <img
         class="max-h-full object-contain"
-        src={data.images[0].href}
+        src={data.images[0].src}
         alt={data.images[0].alt}
       />
     </div>
     <div class="gap-size5 flex w-full flex-col">
       <h4 class="!normal-case">{data.title}</h4>
       <div class="gap-size5 flex items-center justify-between">
-        <p class="text-hue12">€{data.price}</p>
+        <p class="text-hue12">€{data.price.toFixed(2)}</p>
         <CartUpdateButton {data}></CartUpdateButton>
       </div>
     </div>
