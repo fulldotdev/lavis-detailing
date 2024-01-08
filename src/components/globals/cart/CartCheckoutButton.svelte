5079aebd2fc30133bb754da2b74a79b2f2024865
Sil <silveltman@gmail.com>
2024-01-08 16:06:53 +0100

    Fix unit_amount calculation in CartCheckoutButton.svelte

--- src/components/globals/cart/CartCheckoutButton.svelte
+++ src/components/globals/cart/CartCheckoutButton.svelte
@@ -15,7 +15,7 @@
         price_data: {
           currency: 'eur',
           product_data: productData,
-          unit_amount: item.price * 100,
+          unit_amount: Math.round(item.price * 100),
         },
         quantity: item.quantity,
       }
